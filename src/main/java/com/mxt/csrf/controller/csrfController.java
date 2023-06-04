6a2a4b982fe3ba49d8f86e4a669a8b8472eb43c5
Rinth801 <101859138+Rinth801@users.noreply.github.com>
2023-06-04 18:12:45 +0800

    Revert "add CSRF2 GET型"
    This reverts commit 7abacea4fdb27e3b8f816225e7f3f4c482ea923b.

--- src/main/java/com/mxt/csrf/controller/csrfController.java
+++ src/main/java/com/mxt/csrf/controller/csrfController.java
@@ -8,7 +8,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class csrfController {
@@ -21,20 +20,11 @@ public class csrfController {
         return "index";
     }
 
-    @RequestMapping(value="/csrf1",method = RequestMethod.POST)
-    public String addUser1(User user1, Model model) {
+    @RequestMapping("/csrf")
+    public String addUser(User user1, Model model) {
         user.setUserid(user1.getUserid());
         user.setName(user1.getName());
         user.setPassword(user1.getPassword());
-        userService.addUser(user);
-        return "index";
-    }
-
-    @RequestMapping(value="/csrf2",method = RequestMethod.GET)
-    public String addUser2(User user2, Model model) {
-        user.setUserid(user2.getUserid());
-        user.setName(user2.getName());
-        user.setPassword(user2.getPassword());
         userService.addUser(user);
         return "index";
     }
