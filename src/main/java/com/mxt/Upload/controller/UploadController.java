bddb19bc40e1aafd5e7a77526aab3f8b8f3f45b9
JInx <1823299082@qq.com>
2023-06-05 11:40:22 +0800

    add 增加了upload第四关

--- src/main/java/com/mxt/Upload/controller/UploadController.java
+++ src/main/java/com/mxt/Upload/controller/UploadController.java
@@ -1,12 +1,12 @@
 package com.mxt.Upload.controller;
+import org.apache.commons.io.FilenameUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 @Controller
 public class UploadController {
@@ -26,6 +26,7 @@ public class UploadController {
         }
         Map<String ,String > result =new HashMap<>();
         result.put("status","success");
+        result.put("filename", filename);
         if (outfile.exists()) {
             outfile.delete();
         }
@@ -45,10 +46,60 @@ public class UploadController {
         }
         Map<String ,String > result =new HashMap<>();
         result.put("status","success");
+        result.put("filename", filename);
         if (outfile.exists()) {
             outfile.delete();
         }
         return result;
+    }
+
+    @RequestMapping(value = "/upload4", method=RequestMethod.POST)
+    @ResponseBody
+    public Map<String, String> upload4(MultipartFile file) {
+        String filename = file.getOriginalFilename();
+        // 检查文件名是否存在黑名单中字符
+        if (hasBlacklistedExtension(filename)) {
+            filename = removeBlacklistedExtension(filename)+".txt";
+        }
+        // 文件保存路径
+        File outfile = new File(this.path + filename);
+        try {
+            file.transferTo(outfile);
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+
+        Map<String, String> result = new HashMap<>();
+
+        result.put("status", "success");
+        result.put("filename", filename);
+        if (outfile.exists()) {
+            outfile.delete();
+        }
+
+        return result;
+    }
+
+    // 检查文件名是否存在黑名单中字符
+    private boolean hasBlacklistedExtension(String filename) {
+        Set<String> blacklist = new HashSet<>(Arrays.asList("jsp", "php", "exe", "dll", "vxd", "html"));
+        String extension = FilenameUtils.getExtension(filename);
+        return blacklist.contains(extension.toLowerCase());
+    }
+
+    // 将文件名中的黑名单字符替换为空字符串
+    private String removeBlacklistedExtension(String filename) {
+        Set<String> blacklist = new HashSet<>(Arrays.asList("jsp", "php", "exe", "dll", "vxd", "html"));
+        String baseName = FilenameUtils.getBaseName(filename);
+        String[] parts = baseName.split("\\.");
+        StringBuilder newBaseName = new StringBuilder(parts[0]);
+        for (int i = 1; i < parts.length; i++) {
+            if (blacklist.contains(parts[i].toLowerCase())) {
+                continue;
+            }
+            newBaseName.append(".").append(parts[i]);
+        }
+        return newBaseName.toString() + "." + FilenameUtils.getExtension(filename);
     }
 }
 
